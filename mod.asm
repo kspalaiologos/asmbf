4c19d4be50496aee0d7a4db99e93ab1ab58d365f
Παλαιολόγος <marekszuwarek1958@gmail.com>
2019-05-18 22:00:52 +0200

    puts implementation

--- mod.asm
+++ mod.asm
@@ -9,3 +9,16 @@
     sub r2, r3
     pop r3
     ret
+
+; PTR stack[--sp] -> stdout
+:puts
+    pop r1
+:puts_1
+    rcl r2,r1
+    jz_ r2,[puts_2]
+    out r2
+    inc r1
+    jmp [puts_1]
+:puts_2
+    out 10
+    ret

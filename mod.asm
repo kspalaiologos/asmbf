10f1ad7b2c7ed74266ca9b76f7cc87645367d09f
Παλαιολόγος <kspalaiologos@gmail.com>
2019-07-06 21:29:01 +0200

    Broken modulus is no more.

--- mod.asm
+++ mod.asm
@@ -1,15 +1,4 @@
 
-; Modulus polyfill because bfasm's got it broken
-; r1 - n & r2 - x => r2 = n mod x
-:mod
-    psh r3
-    mov r3, r2
-    div r3, r1
-    mul r3, r1
-    sub r2, r3
-    pop r3
-    ret
-
 ; puts()-like function
 ; PTR stack[--sp] -> stdout
 :puts

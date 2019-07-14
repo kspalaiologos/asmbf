8053b9c310282c086144ed50b6415b443f33dfbb
Παλαιολόγος <kspalaiologos@gmail.com>
2019-07-14 15:11:12 +0200

    Macros and inline assembly

--- gisa.c
+++ gisa.c
@@ -4,20 +4,19 @@
 #include <string.h>
 
 enum {
-    LOOP_SYM, WHILE_SYM, EQ_SYM, NEQ_SYM, OUTB_SYM, INB_SYM, /* Keywords */
+    DEFMACRO_SYM, DEFUN_SYM, LOOP_SYM, WHILE_SYM, EQ_SYM, NEQ_SYM, OUTB_SYM, INB_SYM, /* Keywords */
     LPAR, RPAR, LBRA, RBRA, LDER, RDER, /* Symbols */
     PLUS, MINUS, LESS, MUL, DIV, EQUAL, /* Operators */
-    INT, ID, STRING, EOI /* Other */
+    INT, ASM, ID, STRING, EOI /* Other */
 };
 
-char * words[] = {"loop", "while", "eq", "neq", "outb", "inb", NULL};
+char * words[] = {"defmacro", "defun", "loop", "while", "eq", "neq", "outb", "inb", NULL};
 
 int ch = ' ', sym, int_val, line;
 char id_name[128], * str_val;
 
 void syntax_error() {
-    fprintf(stderr, "[stdin:%d] syntax error - token #%d '%c'. next_ch() = '%c'\n", line, sym, ch, next_ch());
-    abort();
+    fprintf(stderr, "[stdin:%d] syntax error - token #%d\n", line, sym); 
 }
 
 void next_ch() {
@@ -25,45 +24,63 @@ void next_ch() {
 }
 
 void next_sym() {
+    int i = 0;
 again:
     switch (ch) {
         case '\n': line++;
-		case '\r':
-		case '\t':
+        case '\r':
+        case '\t':
         case ' ': next_ch(); goto again;
         case EOF: sym = EOI; break;
+        case '@':
+            sym = ASM;
+            next_ch();
+
+            if(ch != '"') {
+                syntax_error();
+            }
+
+            next_ch();
+
+            while(ch != '"') {
+                id_name[i++] = ch;
+                next_ch();
+            }
+
+            next_ch();
+            break;
         case '(': next_ch(); sym = LPAR; break;
         case ')': next_ch(); sym = RPAR; break;
         case '{': next_ch(); sym = LBRA; break;
         case '}': next_ch(); sym = RBRA; break;
-		case '[': next_ch(); sym = LDER; break;
-		case ']': next_ch(); sym = RDER; break;
+        case '[': next_ch(); sym = LDER; break;
+        case ']': next_ch(); sym = RDER; break;
         case '+': next_ch(); sym = PLUS; break;
         case '*': next_ch(); sym = MUL; break;
-		case '/':
-			next_ch();
-			if(ch == '*') {
-				while(1) {
-					next_ch();
-					
-					if(ch == EOF) {
-						fprintf(stderr, "Unterminated comment.");
-						abort();
-					}
-					
-					if(ch == '*') {
-						next_ch();
-						if(ch == '/')
-							break;
-					}
-				}
-				
-				next_ch();
-				
-				goto again;
-			} else
-				sym = DIV;
-			break;
+        case '/':
+            next_ch();
+            if(ch == '*') {
+                while(1) {
+                    next_ch();
+                    
+                    if(ch == EOF) {
+                        fprintf(stderr, "Unterminated comment.");
+                        abort();
+                    }
+                    
+                    if(ch == '*') {
+                        next_ch();
+                        if(ch == '/')
+                            break;
+                    }
+                }
+                
+                next_ch();
+                
+                goto again;
+            } else
+                sym = DIV;
+            break;
         case '-': next_ch(); sym = MINUS; break;
         case '<': next_ch(); sym = LESS; break;
         case '=': next_ch(); sym = EQUAL; break;
@@ -90,7 +107,9 @@ again:
 
 /* Stage 1 Parser. */
 
-enum e_type {PROGRAM, TREE, P_OPERATOR, P_ID, P_KEY, P_ICONST};
+enum {
+    PROGRAM, TREE, P_OPERATOR, P_ID, P_KEY, P_ICONST, P_ASM
+};
 
 typedef struct node {
     struct node ** o;
@@ -123,23 +142,30 @@ node * new_node(int size, int type) {
 node * tree_contents(int type) {
     node * n = new_node(8, TREE);
     int idx = 0, max = 8;
-	
-	n->v = type;
+    
+    n->v = type;
     
     while((type == 1 && sym != RPAR) ||
-	      (type == 2 && sym != RDER) ||
-		  (type == 0 && sym != RBRA)) {
+          (type == 2 && sym != RDER) ||
+          (type == 0 && sym != RBRA)) {
         if(sym == LPAR) {
             n->o[idx++] = tree(1);
             continue;
         }
-		
-		else if(sym == LBRA) {
+
+        else if(sym == ASM) {
+            n->o[idx] = new_node(0, P_ASM);
+            n->o[idx]->s = malloc(strlen(id_name) + 1);
+            if(!n->o[idx]->s) abort();
+            strcpy(n->o[idx++]->s, id_name);
+        }
+        
+        else if(sym == LBRA) {
             n->o[idx++] = tree(0);
             continue;
         }
-		
-		else if(sym == LDER) {
+        
+        else if(sym == LDER) {
             n->o[idx++] = tree(2);
             continue;
         }
@@ -150,7 +176,7 @@ node * tree_contents(int type) {
             if(!n->o[idx]->s) abort();
             strcpy(n->o[idx++]->s, id_name);
         }
-		
+        
         else if(sym == INT) {
             n->o[idx] = new_node(0, P_ICONST);
             n->o[idx++]->v = int_val;
@@ -181,18 +207,18 @@ node * tree_contents(int type) {
 
 node * tree(int type) {
     node * t;
-	
+    
     if ((type == 1 && sym != LPAR) ||
-	    (type == 2 && sym != LDER) ||
-		(type == 0 && sym != LBRA))
+        (type == 2 && sym != LDER) ||
+        (type == 0 && sym != LBRA))
         syntax_error();
-		
+        
     next_sym();
     t = tree_contents(type);
-	
+    
     if ((type == 1 && sym != RPAR) ||
-		(type == 2 && sym != RDER) ||
-		(type == 0 && sym != RBRA))
+        (type == 2 && sym != RDER) ||
+        (type == 0 && sym != RBRA))
         syntax_error();
     next_sym();
     return t;
@@ -201,9 +227,9 @@ node * tree(int type) {
 node * program() {
     node * x = new_node(1, PROGRAM);
     next_sym();
-	
-	x->o[0] = tree(sym == LPAR ? 1 : (sym == LBRA ? 0 : 2));
-	
+    
+    x->o[0] = tree(sym == LPAR ? 1 : (sym == LBRA ? 0 : 2));
+    
     if (sym != EOI)
         syntax_error();
     return x;
@@ -223,50 +249,80 @@ void dump_ast(node * n, int indent) {
 
 /* Code generator. */
 
-int lid = 1;
-int doStack[16], doSP = 0;
+enum {
+    TYPE_VARIABLE, TYPE_MACRO
+};
 
-void gentree(node * x, int c0) {
-	switch(x->o[c0]->type) {
-		case P_ICONST:
-			printf("psh %d\n", x->o[c0]->v);
-			break;
-		case P_OPERATOR:
-			// Todo: Binary operator optimization.
-			if(x->o[c0]->v == PLUS) // Binary plus
-				printf("; Binary addition\npop r5\npop r6\nadd r5, r6\npsh r5\n");
-			if(x->o[c0]->v == MUL) // Binary multiplication
-				printf("; Binary multiplication\npop r5\npop r6\nmul r5, r6\npsh r5\n");
-			if(x->o[c0]->v == LESS) // Binary less-than operator
-				printf("; Binary less-than operator\npop r5\npop r6\nlt_ r5, r6\npsh r5\n");
-			if(x->o[c0]->v == MINUS && x->children == 2) // Unary -
-				printf("; Unary minus\npop r5\nneg r5\npsh r5\n");
-			if(x->o[c0]->v == MINUS && x->children > 2) // Binary -
-				printf("; Binary minus\npop r5\npop r6\nsub r5, r6\npsh r5\n");
-			if(x->o[c0]->v == EQUAL) // =
-				printf("; Binary set\npop r5\npop r6\nsto r5, r6\n");
-			break;
-		case P_KEY:
-			if(!strcmp("outb", x->o[c0]->s))
-				printf("; outb\npop r5\nout r5\n");
-			if(!strcmp("inb", x->o[c0]->s))
-				printf("; inb\nin_ r5\npsh r5\n");
-			if(!strcmp("eq", x->o[c0]->s))
-				printf("; eq\npop r5\npop r6\neq_ r5, r6\npsh r5\n");
-			if(!strcmp("neq", x->o[c0]->s))
-				printf("; neq\npop r5\npop r6\nne_ r5, r6\npsh r5\n");
-			if(!strcmp("loop", x->o[c0]->s))
-				doStack[doSP++] = lid, printf("; loop\nlbl %d\n", lid++);
-			if(!strcmp("while", x->o[c0]->s))
-				printf("; while\npop r5\njnz r5, %d\n", doStack[--doSP]);
-			break;
-		case TREE:
-			c(x->o[c0]);
-			break;
-		default:
-			fprintf(stderr, "Unexpected %d.\n", x->o[c0]->type);
-			abort();
-	}
+typedef struct symbol {
+    int type;
+    char * contents, * name;
+    node * macro;
+} symbol;
+
+int lid = 1;
+int doStack[16], doSP = 0, lastGlob = 0;
+symbol globs[256];
+
+void c(node * x);
+
+int gentree(node * x, int c0) {
+    int c1;
+    switch(x->o[c0]->type) {
+        case P_ICONST:
+            printf("psh %d\n", x->o[c0]->v);
+            break;
+        case P_OPERATOR:
+            // Todo: Binary operator optimization.
+            if(x->o[c0]->v == PLUS) // Binary plus
+                printf("; Binary addition\npop r5\npop r6\nadd r5, r6\npsh r5\n");
+            if(x->o[c0]->v == MUL) // Binary multiplication
+                printf("; Binary multiplication\npop r5\npop r6\nmul r5, r6\npsh r5\n");
+            if(x->o[c0]->v == LESS) // Binary less-than operator
+                printf("; Binary less-than operator\npop r5\npop r6\nlt_ r5, r6\npsh r5\n");
+            if(x->o[c0]->v == MINUS && x->children == 2) // Unary -
+                printf("; Unary minus\npop r5\nneg r5\npsh r5\n");
+            if(x->o[c0]->v == MINUS && x->children > 2) // Binary -
+                printf("; Binary minus\npop r5\npop r6\nsub r5, r6\npsh r5\n");
+            if(x->o[c0]->v == EQUAL) // =
+                printf("; Binary set\npop r5\npop r6\nsto r5, r6\n");
+            break;
+        case P_KEY:
+            if(!strcmp("outb", x->o[c0]->s))
+                printf("; outb\npop r5\nout r5\n");
+            else if(!strcmp("inb", x->o[c0]->s))
+                printf("; inb\nin_ r5\npsh r5\n");
+            else if(!strcmp("eq", x->o[c0]->s))
+                printf("; eq\npop r5\npop r6\neq_ r5, r6\npsh r5\n");
+            else if(!strcmp("neq", x->o[c0]->s))
+                printf("; neq\npop r5\npop r6\nne_ r5, r6\npsh r5\n");
+            else if(!strcmp("loop", x->o[c0]->s))
+                doStack[doSP++] = lid, printf("; loop\nlbl %d\n", lid++);
+            else if(!strcmp("while", x->o[c0]->s))
+                printf("; while\npop r5\njnz r5, %d\n", doStack[--doSP]);
+            break;
+        case TREE:
+            c(x->o[c0]);
+            break;
+        case P_ID:
+            for(c1 = 0; c1 < lastGlob; c1++) {
+                if(globs[c1].name != NULL && !strcmp(globs[c1].name, x->o[c0]->s)) {
+                    if(globs[c1].macro == NULL) {
+                        fprintf(stderr, "%s is not a macro", globs[c1].name);
+                    }
+					
+                    c(globs[c1].macro);
+                }
+            }
+            break;
+        case P_ASM:
+            printf("%s\n", x->o[c0]->s);
+            break;
+        default:
+            fprintf(stderr, "Unexpected %d.\n", x->o[c0]->type);
+            abort();
+    }
+    
+    return 0;
 }
 
 void c(node * x) {
@@ -282,21 +338,53 @@ void c(node * x) {
             break;
         
         case TREE:
-			if(x->v == 1) {
-				c0 = x->children;
-				while(--c0 >= 0)
-					gentree(x, c0);
-			} else if(x->v == 0) {
-				c0 = 0;
-				while(c0 < x->children)
-					gentree(x, c0++);
-			} else {
-				c0 = x->children;
-				while(--c0 >= 0)
-					gentree(x, c0);
-				printf("; deref\npop r5\nrcl r6, r5\npsh r6\n");
-			}
-			
+            if(x->children >= 1) {
+                node * n = x->o[x->v != 0 ? (x->children - 1) : 0];
+                if(n->type == P_KEY) {
+                    if(!strcmp("defmacro", n->s)) {
+                        if(x->children != 3) {
+                            fprintf(stderr, "Three macro children expected.");
+                            abort();
+                        }
+
+                        if(x->o[x->v != 0 ? (x->children - 2) : 1]->type != P_ID) {
+                            fprintf(stderr, "Second macro child is not an identifier");
+                            abort();
+                        }
+
+                        if(x->o[x->v != 0 ? (x->children - 3) : 2]->type != TREE) {
+                            fprintf(stderr, "Third macro child is not a tree");
+                            abort();
+                        }
+
+                        globs[lastGlob].type = TYPE_MACRO;
+                        globs[lastGlob].macro = x->o[x->v != 0 ? (x->children - 3) : 1];
+                        globs[lastGlob++].name = x->o[x->v != 0 ? (x->children - 2) : 2]->s;
+                        break;
+                    } else if(!strcmp("defun", n->s)) {
+                        break;
+                    }
+                }
+            }
+
+            if(x->v == 1) {
+                c0 = x->children;
+                while(--c0 >= 0)
+                    if(gentree(x, c0))
+                        break;
+            } else if(x->v == 0) {
+                c0 = 0;
+                while(c0 < x->children)
+                    if(gentree(x, c0++))
+                        break;
+            } else {
+                c0 = x->children;
+                while(--c0 >= 0)
+                    if(gentree(x, c0))
+                        break;
+                printf("; deref\npop r5\nrcl r6, r5\npsh r6\n");
+            }
+            
             break;
     }
 }
@@ -306,4 +394,4 @@ void c(node * x) {
 int main() {
     c(program());
     return 0;
-}
+} 

f36cc4c4c497f565869dd4890b0f9ff9e9b6cea2
Παλαιολόγος <kspalaiologos@gmail.com>
2019-07-16 18:09:37 +0200

    Indentation seems right now.

--- gisa.c
+++ gisa.c
@@ -46,8 +46,8 @@ again:
                 id_name[i++] = ch;
                 next_ch();
             }
-			
-			id_name[i++] = 0;
+            
+            id_name[i++] = 0;
 
             next_ch();
             break;
@@ -291,7 +291,7 @@ int gentree(node * x, int c0) {
                 printf("; Binary set\npop r2\nadd r2, %d\npop r3\nsto r2, r3\n", CODE_OFFSET);
             break;
         case P_KEY:
-			if(!strcmp("outb", x->o[c0]->s))
+            if(!strcmp("outb", x->o[c0]->s))
                 printf("; outb\npop r2\nout r2\n");
             else if(!strcmp("inb", x->o[c0]->s))
                 printf("; inb\nin_ r2\npsh r2\n");
@@ -312,15 +312,15 @@ int gentree(node * x, int c0) {
                 if(globs[c1].name != NULL && !strcmp(globs[c1].name, x->o[c0]->s)) {
                     if(globs[c1].macro == NULL) {
                         fprintf(stderr, "%s is not a macro", globs[c1].name);
-						abort();
-					} else
-						c2++, c(globs[c1].macro);
+                        abort();
+                    } else
+                        c2++, c(globs[c1].macro);
                 }
             }
             
             if(!c2) {
                 fprintf(stderr, "Macro not found: %s\n", globs[c1].name);
-				abort();
+                abort();
             }
             break;
         case P_ASM:
@@ -347,8 +347,8 @@ void c(node * x) {
             break;
         
         case TREE:
-			c3 = 0;
-			
+            c3 = 0;
+            
             if(x->children >= 1) {
                 node * n = x->o[x->v != 0 ? (x->children - 1) : 0];
                 if(n->type == P_KEY) {
@@ -373,86 +373,86 @@ void c(node * x) {
                         globs[lastGlob++].name = x->o[x->v != 0 ? (x->children - 2) : 2]->s;
                         break;
                     } else if(!strcmp("defun", n->s)) {
-						node * chnode;
-						int nparam;
-						
-						if(x->children != 3) {
-							fprintf(stderr, "Three defun children expected.");
-							abort();
-						}
-						
-						if(x->o[x->v != 0 ? 2 : 1]->type != P_ID) {
-							fprintf(stderr, "Second defun child is not an identifier");
-							abort();
-						}
-						
-						if(x->o[x->v != 0 ? 1 : 2]->type != TREE) {
+                        node * chnode;
+                        int nparam;
+                        
+                        if(x->children != 3) {
+                            fprintf(stderr, "Three defun children expected.");
+                            abort();
+                        }
+                        
+                        if(x->o[x->v != 0 ? 2 : 1]->type != P_ID) {
+                            fprintf(stderr, "Second defun child is not an identifier");
+                            abort();
+                        }
+                        
+                        if(x->o[x->v != 0 ? 1 : 2]->type != TREE) {
                             fprintf(stderr, "Third macro child is not a tree");
                             abort();
                         }
-						
-						globs[lastGlob].type = TYPE_FUNC;
-						globs[lastGlob].macro = NULL;
-						globs[lastGlob].id = lid++;
-						globs[lastGlob++].name = x->o[x->v != 0 ? 2 : 1]->s;
-						
-						printf("; function decl\nlbl %d\n", globs[lastGlob-1].id);
-						c(x->o[x->v != 0 ? 1 : 2]);
-						printf("pop r1\nret\n");
-						
+                        
+                        globs[lastGlob].type = TYPE_FUNC;
+                        globs[lastGlob].macro = NULL;
+                        globs[lastGlob].id = lid++;
+                        globs[lastGlob++].name = x->o[x->v != 0 ? 2 : 1]->s;
+                        
+                        printf("; function decl\nlbl %d\n", globs[lastGlob-1].id);
+                        c(x->o[x->v != 0 ? 1 : 2]);
+                        printf("pop r1\nret\n");
+                        
                         break;
                     } else if(!strcmp("invoke", n->s)) {
-						int rad = lid++;
-						
-						if(x->children != 3) {
-							fprintf(stderr, "Three invoke children expected.");
-							abort();
-						}
-						
-						if(x->o[x->v != 0 ? 1 : 1]->type != P_ID) {
-							fprintf(stderr, "Second invoke child is not a P_ID.");
-							abort();
-						}
-						
-						if(x->o[x->v != 0 ? 0 : 1]->type != TREE) {
-							fprintf(stderr, "Third invoke child is not a tree.");
-							abort();
-						}
-						
-						printf("; save return address\npsh %d\n", rad);
-						
-						printf("; evaluate parameter tree\n");
-						c(x->o[x->v != 0 ? 0 : 1]);
-						
-						for(c1 = 0, c2 = 0; c1 < lastGlob; c1++) {
-							if(globs[c1].name != NULL && !strcmp(globs[c1].name, x->o[x->v != 0 ? 1 : 1]->s)) {
-								if(globs[c1].macro != NULL) {
-									fprintf(stderr, "%s is a macro", globs[c1].name);
-									abort();
-								} else
-									c2++, c0 = globs[c1].id;
-							}
-						}
-						
-						if(!c2) {
-							fprintf(stderr, "Function not found: %s\n", x->o[x->v != 0 ? 1 : 1]->s);
-							abort();
-						}
-						
-						printf("; invoke\njmp %d\nlbl %d\npsh r1\n", c0, rad);
-						
-						break;
-					} else if(!strcmp("exports", n->s)) {
-						if(x->children < 2) {
-							fprintf(stderr, "At least two `exports` children expected.");
-							abort();
-						}
-						
-						c3 = lid++;
-						
-						
-						printf("; exports\njmp %d\n", c3);
-					}
+                        int rad = lid++;
+                        
+                        if(x->children != 3) {
+                            fprintf(stderr, "Three invoke children expected.");
+                            abort();
+                        }
+                        
+                        if(x->o[x->v != 0 ? 1 : 1]->type != P_ID) {
+                            fprintf(stderr, "Second invoke child is not a P_ID.");
+                            abort();
+                        }
+                        
+                        if(x->o[x->v != 0 ? 0 : 1]->type != TREE) {
+                            fprintf(stderr, "Third invoke child is not a tree.");
+                            abort();
+                        }
+                        
+                        printf("; save return address\npsh %d\n", rad);
+                        
+                        printf("; evaluate parameter tree\n");
+                        c(x->o[x->v != 0 ? 0 : 1]);
+                        
+                        for(c1 = 0, c2 = 0; c1 < lastGlob; c1++) {
+                            if(globs[c1].name != NULL && !strcmp(globs[c1].name, x->o[x->v != 0 ? 1 : 1]->s)) {
+                                if(globs[c1].macro != NULL) {
+                                    fprintf(stderr, "%s is a macro", globs[c1].name);
+                                    abort();
+                                } else
+                                    c2++, c0 = globs[c1].id;
+                            }
+                        }
+                        
+                        if(!c2) {
+                            fprintf(stderr, "Function not found: %s\n", x->o[x->v != 0 ? 1 : 1]->s);
+                            abort();
+                        }
+                        
+                        printf("; invoke\njmp %d\nlbl %d\npsh r1\n", c0, rad);
+                        
+                        break;
+                    } else if(!strcmp("exports", n->s)) {
+                        if(x->children < 2) {
+                            fprintf(stderr, "At least two `exports` children expected.");
+                            abort();
+                        }
+                        
+                        c3 = lid++;
+                        
+                        
+                        printf("; exports\njmp %d\n", c3);
+                    }
                 }
             }
 
@@ -473,10 +473,10 @@ void c(node * x) {
                         break;
                 printf("; deref\npop r2\nadd r2, %d\nrcl r3, r2\npsh r3\n", CODE_OFFSET);
             }
-			
-			if(c3) {
-				printf("; export end block\nlbl %d\n", c3);
-			}
+            
+            if(c3) {
+                printf("; export end block\nlbl %d\n", c3);
+            }
             
             break;
     }

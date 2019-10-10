13e9f11901ffe6f179c10bb16aec3e0b305e7d52
Παλαιολόγος <kspalaiologos@gmail.com>
2019-10-10 18:49:41 +0200

    Create labels.pl

---
+++ labels.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# asm2bf label preprocessor.
+# C'mon, who doesn't love regexes?
+
+# Released to the public domain by Krzysztof Szewczyk.
+
+$n=0;%o=();$_=do{local$/;<>};s/^[ \t]*@([A-Za-z_]+).*$/$o{$1}=++$n;'lbl '.$n."\n";/gem;
+s/(%[A-Za-z]+|"[^"\n]*%[A-Za-z]+)/substr($1,0,1)eq'"'?$1:%o{substr$1,1}/ge;print;

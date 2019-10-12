bf3305e66f693f2e37e14d8c9d8b61605728083a
Παλαιολόγος <kspalaiologos@gmail.com>
2019-10-12 18:58:12 +0200

    Verbose test output

--- test.pl
+++ test.pl
@@ -3,13 +3,17 @@
 $file = $ARGV[0];
 `$file`;
 
+print "Test:\t$file\n";
+
 $file  =~ s{\.[^.]+$}{};
+
+print "Running:\t$file.b < $file.in > $file.aout, comparing with $file.aout\n";
 
 `bfi $file.b < $file.in > $file.aout`;
 $diff = `diff $file.aout $file.out`;
 
 if(length($diff) > 0) {
-	print " *** TEST FAILED! Output diff:\n";
+	print " *** TEST FAILED!\tOutput diff:\n";
 	print $diff;
 }
 

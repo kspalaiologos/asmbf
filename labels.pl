85679e6764f1c120f214d3f4cd17f1518aa2d1d8
Παλαιολόγος <kspalaiologos@gmail.com>
2019-11-18 15:13:44 +0100

    Digits are allowed in label names. (#24)

--- labels.pl
+++ labels.pl
@@ -5,5 +5,5 @@
 
 # Released to the public domain by Krzysztof Szewczyk.
 
-$n=0;%o=();$_=do{local$/;<>};s/^[ \t]*@([A-Za-z_]+).*$/$o{$1}=++$n;'lbl '.$n."\n";/gem;
-s/(%[A-Za-z_]+|"[^"\n]*%[A-Za-z_]+)/substr($1,0,1)eq'"'?$1:%o{substr$1,1}/ge;print;
+$n=0;%o=();$_=do{local$/;<>};s/^[ \t]*@([A-Za-z_]+[A-Za-z0-9_]*).*$/$o{$1}=++$n;'lbl '.$n."\n";/gem;
+s/(%([A-Za-z_]+[A-Za-z0-9_]*)|"[^"\n]*%([A-Za-z_]+[A-Za-z0-9_]*))/substr($1,0,1)eq'"'?$1:%o{substr$1,1}/ge;print;
---
+++ test/label-with-digit.asm
@@ -0,0 +1,9 @@
+#!/bin/bfmake
+
+jmp %hello1text3
+end
+
+@hello1text3
+  mov r1, %hello1text3
+  add r1, 48
+  out r1

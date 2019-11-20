3d1064d8489e2c897231264e7ae41503c1a59a58
Παλαιολόγος <kspalaiologos@gmail.com>
2019-11-20 17:30:44 +0100

    Improve the functionality of program.
    The credit is overall even larger than the entire file and the code is so simple, that there is no need for crediting anyone. Also, patches have been made.

--- derle.pl
+++ derle.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl -p
-# Credit goes to https://codegolf.stackexchange.com/a/146919/61379
 
-s/\d+(.)/$1x$&/ge
+my ($style) = @ARGV;
+
+s/\d+(.)/$1x$&/ge if $style eq "postfix" or $style == undef;
+s/(.)\d+/$1x$&/ge if $style eq "prefix";

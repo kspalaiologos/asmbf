591e797ce103ca2f2872021c61984d5300fe1b72
Παλαιολόγος <kspalaiologos@gmail.com>
2019-12-31 16:03:49 +0100

    Update labels.pl

--- labels.pl
+++ labels.pl
@@ -3,7 +3,5 @@
 # asm2bf label preprocessor.
 # C'mon, who doesn't love regexes?
 
-# Released to the public domain by Kamila Szewczyk.
-
 $n=0;%o=();$_=do{local$/;<>};s/^[ \t]*@([A-Za-z_]+[A-Za-z0-9_]*).*$/$o{$1}=++$n;'lbl '.$n."\n";/gem;
 s/(%([A-Za-z_]+[A-Za-z0-9_]*)|"[^"\n]*%([A-Za-z_]+[A-Za-z0-9_]*))/substr($1,0,1)eq'"'?$1:%o{substr$1,1}/ge;print;

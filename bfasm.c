13585f2cda607dcbb89382c467cdd6c3aabff5e3
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2019-12-31 12:01:25 +0100

    2 more registers? hopefully?

--- bfasm.c
+++ bfasm.c
@@ -158,7 +158,7 @@ Lax:;
     if (m[0] != 'r' && m[0] != 'R') goto Lak; /* r_ operand */
     m[0] = inchar();
     m[0] = m[0] - '1';
-    if (m[0] > 4) goto Laz; /* not a number, quit */
+    if (m[0] > 6) goto Laz; /* not a number, quit */
     m[4] = m[0] != 4 ? (m[0] < 4 ? (m[0] + 'f') : (m[0] + 'q')) : 'm';
     goto Laa;
 Lak:;

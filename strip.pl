ba9f483e83bd49f0631145b02670125b268f1489
Παλαιολόγος <marekszuwarek1958@gmail.com>
2020-01-18 20:58:17 +0100

    hotfix: semicolon, the hide and seek champion

--- strip.pl
+++ strip.pl
@@ -4,7 +4,7 @@ my ($style) = @ARGV;
 $_ = do { local $/; <> };
 do {
 	$l = length;
-	s/[\n\r\t ]+//g
+	s/[\n\r\t ]+//g;
 	s/[^\+\-\<\>\[\]\.\,]+//g if $style eq "--generic" or $style == undef;
 	s/[^\+\-\<\>\[\]\.\,0-9]+//g if $style eq "--keep-rle" or $style == undef;
 	s/<>//g;

98952fd1c089e3e4862cf050a1d552f4e160e3e8
Παλαιολόγος <marekszuwarek1958@gmail.com>
2020-01-18 20:55:34 +0100

    --keep-rle / --generic flags.

--- strip.pl
+++ strip.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/perl
 
+my ($style) = @ARGV;
 $_ = do { local $/; <> };
 do {
 	$l = length;
-	s/[\n\r\t ]+//g;
+	s/[\n\r\t ]+//g if $style eq "--generic" or $style == undef;
+	s/[\n\r\t0-9 ]+//g if $style eq "--keep-rle" or $style == undef;
 	s/<>//g;
 	s/><//g;
 	s/\+-//g;
 	s/-\+//g;
 } while($l != length);
+
 print;

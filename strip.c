1c0241c60d9878aa44172b69ff3ab72a5f67c1af
Παλαιολόγος <kspalaiologos@gmail.com>
2020-02-06 16:14:37 +0100

    Update strip.c

--- strip.c
+++ strip.c
@@ -1,6 +1,5 @@
-#define a(v,l,r)s=v<0?l:r;v=abs(v);while(v--)putchar(s);v=0;
-x,y,s,r,c;main(int argc,char**argv){r=argc==2&&strcmp("--kee"
-"p-rle",argv[1]);while((c=getchar())!=-1){if(isspace(c)||!strchr
-(9*r+"0123456789+-<>[].,",c));else if(c=='>'||c=='<'){a(y,'-'
-,'+');x+=c=='>'?1:-1;}else if(c=='+'||c=='-'){a(x,'<','>');y+=
-c=='+'?1:-1;}else{a(y,'-','+');a(x,'<','>');putchar(c);}}}
+#define a(v,l,r,z)({s=v<0?l:r;v=abs(v);while(v--)putchar(s);v=0;z;})
+x,y,s,r,c;main(int n,char**p){r=n==2&&strcmp("--keep-rle",p+1);while
+(~(c=getchar()))!isspace(c)&&strchr(9*r+"0123456789+-<>[].,",c)?c-60
+&~2?c-43&~2?a(y,45,43,a(x,60,62,putchar(c))):a(x,60,62,y+=44-c):a(y,
+45,43,x+=c-61):0;}

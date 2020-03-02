ea0f19d448993d8f7bd7bd86a6bb4967fdb61ec7
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-03-02 17:30:18 +0100

    brand new include()!

--- lib-bfm.lua
+++ lib-bfm.lua
@@ -1,7 +1,22 @@
 
 $(
 
-function include()
+function slurp(file)
+    local f = assert(io.open(file, "rb"))
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+function include(path)
+	-- so essentially we'd like to pipe the output
+	-- of /bin/bfpp and return it
+	
+	local file = assert(io.popen('/bin/bfpp', 'rw'))
+	file:write(slurp(path))
+	local output = file:read('*all')
+	file:close()
+	return output
 end
 
 )

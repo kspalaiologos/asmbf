44e8056bc0d54744b569ef66ce4e9ccbf1397094
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-04-14 13:01:39 +0200

    div comment, comparasion pass barebones

---
+++ rave/src/rave/pass/ComparisonPass.java
@@ -0,0 +1,21 @@
+package rave.pass;
+
+import java.util.List;
+
+import rave.nodes.INode;
+
+public class ComparisonPass implements IPass {
+
+	@Override
+	public int match(List<INode> input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public INode build(List<INode> input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

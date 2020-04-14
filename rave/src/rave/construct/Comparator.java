6961dc8984af5ca8fab321cf5e3617e9595149c1
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-04-14 15:13:38 +0200

    comparator implementation

--- rave/src/rave/construct/Comparator.java
+++ rave/src/rave/construct/Comparator.java
@@ -1,5 +1,7 @@
 package rave.construct;
 
+import java.util.function.BinaryOperator;
+
 import rave.common.Automata;
 import rave.nodes.MorphableNode;
 import rave.nodes.WithoutCoefficients;
@@ -8,8 +10,14 @@ public class Comparator extends MorphableNode implements WithoutCoefficients {
 
 	@Override
 	public void execute(Automata state) {
-		// TODO Auto-generated method stub
+		BinaryOperator<Integer> comparator = (BinaryOperator<Integer>) settings().get("comparator");
+		Integer d1 = (Integer) settings.get("delta_1"), d2 = (Integer) settings.get("delta_2");
+		Integer abs_mp = (Integer) settings.get("abs_mp");
 		
+		Integer result = comparator.apply(state.tape.get(state.mp.getValue() + d2 + d1), state.tape.get(state.mp.getValue() + d1));
+		
+		state.tape.set(state.mp.getValue() + d1, result);
+		state.mp.setValue(abs_mp);
 	}
 
 }

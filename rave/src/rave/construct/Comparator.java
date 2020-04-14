a5a7ea912841190476f549a68f6ed21e9d498de4
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-04-14 13:46:20 +0200

    comparator stub

---
+++ rave/src/rave/construct/Comparator.java
@@ -0,0 +1,15 @@
+package rave.construct;
+
+import rave.common.Automata;
+import rave.nodes.MorphableNode;
+import rave.nodes.WithoutCoefficients;
+
+public class Comparator extends MorphableNode implements WithoutCoefficients {
+
+	@Override
+	public void execute(Automata state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

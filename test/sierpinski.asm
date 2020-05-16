cc8591fa7f8d83cbcbb38a6421fdf522eb698c8a
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-05-16 10:25:47 +0200

    sierpinski triangles test

---
+++ test/sierpinski.asm
@@ -0,0 +1,21 @@
+
+@loop1
+	mov r2, 0
+	
+	@loop2
+		mov r3, r1
+		band r3, r2
+		cge r3, 1
+		mov r3, .*
+		cmov r3, 32
+		out r3
+		
+		clt r2, 63
+		cadd r2, 1
+		cjn %loop2
+	
+	out 10
+	
+	clt r1, 63
+	cadd r1, 1
+	cjn %loop1

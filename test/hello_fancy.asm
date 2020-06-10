66f5b3456081f0c87a31efcafa0a4b14120017cf
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-06-10 15:06:18 +0200

    hello_fancy test

---
+++ test/hello_fancy.asm
@@ -0,0 +1,25 @@
+#!/bin/asmbf/bfmake
+
+; A fancy hello world program utilizing the CALLIB, and the newest label features
+; including data labels and other goodies we've got!
+
+stk 2
+org 0
+
+&string
+txt "Hello World!"
+db 0
+
+mov r1, *string
+#call("puts")
+
+end
+
+@puts
+    rcl r2, r1
+    jz r2, %puts_quit
+    out r2
+    inc r1
+    jmp %puts
+@puts_quit
+    out 10

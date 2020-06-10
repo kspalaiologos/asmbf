5899006c72475d1b928cdaab6d0f7ec01daaa21e
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-06-10 14:35:24 +0200

    fsub patch; fsub/fdiv tests

---
+++ test/fdiv.asm
@@ -0,0 +1,17 @@
+
+stk 20
+
+psh 4
+psh 1
+
+psh 4
+psh 3
+
+; 3/4 / 1/4 = 3/4 * 4/1 = 12/4 = 3/1
+
+fdiv
+freduce
+
+pop r1
+add r1, .0
+out r1
---
+++ test/fsub.asm
@@ -0,0 +1,15 @@
+
+stk 20
+
+psh 4
+psh 1
+
+psh 4
+psh 3
+
+
+fsub
+
+pop r1
+add r1, .0
+out r1

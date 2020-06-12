6e7dc97abddb2c45396bfd04295eecf8424a8390
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-06-12 19:03:00 +0200

    corle

--- bfasm.c
+++ bfasm.c
@@ -76,6 +76,7 @@ int bfasm(void) {
 	"x0A"
 	"x0B"
 	"x0C"
+	"x0D"
 	"stkorgdb_txtrawseg"
 			   
                "a+b+[<[>-]>[>]<\0" /* 0 first */
@@ -179,6 +180,7 @@ int bfasm(void) {
 "1[-c+d+1]d[-1+d]c[m+c-]2[n+2-]m[2+m>[-<]>]<<[[-]c+2-m<<]>>n[2+n-]q[e+q[-]]e[-c[q+c[e+c-]]e[c+e-]]c[-]\0" /* candgt */
 "1[-c+d+1]d[-1+d]c[2-e+c-]+2[c-2[e+2-]]e[2+e-]q[e-q[-]]+e+[-q-c[q+[e+c-]]e[c+e-]]c[-]\0" /* coreq */
 "1[-c+d+1]c[-1+c]d[2-c+d-]2[d+2[c+2-]]c[2+c-]q[e-q[-]]+e+[-q-d[q+[e+d-]]e[d+e-]]t 5d[-]\0" /* corne */
+"1[-c+d+1]d[-1+d]c[m+c-]+2[n+2-]m[2+m>[-<]>]<<[[-]c-2-m<<]>>n[2+n-]q[e-q[-]]+e+[-q-c[q+[e+c-]]e[c+e-]]c[-]\0" /* corle */
 		   ;
     for (n = 0; n < 7000; n++)  m[n + 20] = s[n];
     m[6] = 0;

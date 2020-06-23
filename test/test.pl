3a5bbcbb3f3f4e98f1047786b475846f559df762
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-06-23 15:21:37 +0200

    dsc test, test patches

--- test/test.pl
+++ test/test.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
+use Term::ANSIColor;
+
 foreach my $file(@ARGV) {
 	system("$ENV{'HOME'}/.asmbf/bfmake $file");
 
-	print "$file\t";
+	printf "%-40s", $file;
 
 	$file  =~ s{\.[^.]+$}{};
 
@@ -11,12 +13,16 @@ foreach my $file(@ARGV) {
 	$diff = `diff $file.aout $file.out`;
 
 	if(length($diff) > 0) {
+        print color('bold red');
 		print " *** TEST FAILED!\tOutput diff:\n";
 		print $diff;
 		exit length($diff);
 	} else {
+        print color('bold green');
 		print " *** TEST PASS.\n";
 	}
+    
+    print color('reset');
 }
 
 exit 0;

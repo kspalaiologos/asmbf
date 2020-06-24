20cd6ab37b143dc860441f694bb1b33b1918be40
Krzysztof Szewczyk <kspalaiologos@gmail.com>
2020-06-24 10:36:53 +0200

    sgt, spt, sle

---
+++ test/sgt.asm
@@ -0,0 +1,8 @@
+
+psh 2
+psh 3
+psh 4
+
+sgt r1, 2
+add r1, .0
+out r1
---
+++ test/sle.asm
@@ -0,0 +1,32 @@
+psh 3
+psh 4
+psh 5
+psh 6
+psh 7
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+psh 8
+
+sle r1
+add r1, .0
+out r1

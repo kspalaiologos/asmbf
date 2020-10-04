097c5e7ebf6ceaaaa654de320ae4f21ff902e342
Kamila Szewczyk <kspalaiologos@gmail.com>
2020-10-04 17:43:35 +0200

    parallel tests

--- test/test.pl
+++ test/test.pl
@@ -2,27 +2,44 @@
 
 use Term::ANSIColor;
 
+my @proc;
+
 foreach my $file(@ARGV) {
-    system("$ENV{'HOME'}/.asmbf/bfmake $file");
+    my $pid = fork();
+    push @proc, $pid;
 
-    printf "%-40s", $file;
+    if ($pid == 0) {
+        system("$ENV{'HOME'}/.asmbf/bfmake $file");
 
-    $file  =~ s{\.[^.]+$}{};
+        $file  =~ s{\.[^.]+$}{};
 
-    `timeout 20s $ENV{'HOME'}/.asmbf/bfi $file.b < $file.in > $file.aout`;
-    $diff = `diff $file.aout $file.out`;
+        `timeout 20s $ENV{'HOME'}/.asmbf/bfi $file.b < $file.in > $file.aout`;
+        $diff = `diff $file.aout $file.out`;
 
-    if(length($diff) > 0) {
-        print color('bold red');
-        print " *** TEST FAILED!\tOutput diff:\n";
-        print $diff;
-        exit length($diff);
-    } else {
-        print color('bold green');
-        print " *** TEST PASS.\n";
+        if(length($diff) > 0) {
+            printf "%-40s", $file;
+            print color('bold red');
+            print " *** TEST FAILED!\tOutput diff:\n";
+            print $diff;
+            print color('reset');
+            exit length($diff);
+        } else {
+            printf "%-40s", $file;
+            print color('bold green');
+            print " *** TEST PASS.";
+            print color('reset');
+            print "\n";
+        }
+        
+        exit 0;
     }
-    
-    print color('reset');
+}
+
+while(($pid = wait()) > 0){
+    if($? > 0) {
+        kill 'TERM', @proc;
+        exit 1;
+    }
 }
 
 exit 0;

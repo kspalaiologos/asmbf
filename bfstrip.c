494c6dac82a9833c7d5dbe46ec0e31f86d8e116f
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-04-17 19:50:51 +0200

    bfstrip: flag to not strip comments

--- bfstrip.c
+++ bfstrip.c
@@ -15,13 +15,16 @@ static unsigned int col;
     col++; \
 } while(0);
 
-void process(int c, int type, int lw) {
+void process(int ni, int c, int type, int lw) {
     long i;
     long count = c - type;
 
     while ((c = getchar()) != EOF) {
         if(!strchr("+-<>[].,", c))
-            continue;
+            if(ni) {
+                ungetc(c, stdin); break;
+            } else
+                continue;
 
         if ((c != type + 1) && (c != type - 1)) {
             ungetc(c, stdin);
@@ -44,17 +47,17 @@ void process(int c, int type, int lw) {
 }
 
 int main(int argc, char * argv[]) {
-    int c, lw = -1;
+    int c, lw = -1, ni = 0;
 
-    if(argc == 2) {
-        lw = atoi(argv[1]);
-    }
+    if(argc >= 2 && !strcmp(argv[1], "--no-ignore")) { ni = 1; argc--; argv++; }
+    if(argc == 2) lw = atoi(argv[1]);
 
     while ((c = getchar()) != EOF) {
         switch (c) {
-            case '<': case '>': process(c, '=', lw); break;
-            case '+': case '-': process(c, ',', lw); break;
+            case '<': case '>': process(ni, c, '=', lw); break;
+            case '+': case '-': process(ni, c, ',', lw); break;
             case '[': case ']': case ',': case '.': wrap_putchar(c); break;
+            default: if(ni) putchar(c);
         }
     }
 }

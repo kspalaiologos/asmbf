d0c3cd2f56066f4debf0a53cf6f328e39057d927
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-06-17 13:28:41 +0200

    better sk

--- examples/tiny_sk.asm
+++ examples/tiny_sk.asm
@@ -1,4 +1,13 @@
 
+?AX=r1
+?BX=r2
+?CX=r3
+?DX=r4
+?EX=r5
+?FX=r6
+?GX=f2
+?HX=f3
+
 stk 38
 org 0
 
@@ -9,173 +18,167 @@ db 2
 
 $(
     function def_inline(ins)
-        print("@" .. ins .. "_i\n" .. ins .. " f2, f3\nret\n")
+        print("@" .. ins .. "_i\n" .. ins .. " GX, HX\nret\n")
     end
 
     function inline(ins, arg1, arg2)
-        print("mov f2, " .. arg1 .. "\nmov f3, " .. arg2 .. "\n")
+        print("mov GX, " .. arg1 .. "\nmov HX, " .. arg2 .. "\n")
         call(ins .. "_i")
     end
 
     function inline_r(ins, arg1, arg2)
-        print("mov f3, " .. arg2 .. "\n")
+        print("mov HX, " .. arg2 .. "\n")
         call(ins .. "_i")
-        print("mov " .. arg1 .. ", f2")
+        print("mov " .. arg1 .. ", GX")
     end
 )
 
 #call("read")
 @loop
 #call("eval")
-    $(inline_r("rcl", "r2", "*changed"))
-    mov f2, *changed
-    mov f3, 0
+    $(inline_r("rcl", "BX", "*changed"))
+    mov GX, *changed
+    mov HX, 0
 #call("sto_i")
-    ^jnz r2, %loop
+    ^jnz BX, %loop
     ^push 0
 @P
-    ^push r2
-    add r1, 2
-    $(inline_r("rcl", "r2", "r1"))
-    sub r1, 2
-    jz r2, %PS
-    dec r2
-    jz r2, %PK
+    ^push BX
+    add AX, 2
+    $(inline_r("rcl", "BX", "AX"))
+    sub AX, 2
+    jz BX, %PS
+    dec BX
+    jz BX, %PK
     out .(
-    ^mov r2, r1
-    $(inline_r("rcl", "r1", "r2"))
+    ^mov BX, AX
+    $(inline_r("rcl", "AX", "BX"))
 #call("P")
-    inc r2
-    $(inline_r("rcl", "r1", "r2"))
+    inc BX
+    $(inline_r("rcl", "AX", "BX"))
 #call("P")
     out .)
-    pop r2
+    pop BX
     ret
 @PS
     out .S
-    pop r2
+    pop BX
     ret
 @PK
     out .K
-    pop r2
+    pop BX
     ret
 @A
-    mov f3, *heap
+    mov HX, *heap
 #call("rcl_i")
-    add f2, 2
-    ^mov f3, r1
+    add GX, 2
+    ^mov HX, AX
 #call("sto_i")
-    inc f2
-    ots f2, *heap
-    sub f2, 3
-    ^mov r1, f2
+    inc GX
+    ots GX, *heap
+    sub GX, 3
+    ^mov AX, GX
     ret
 @read
-    ^push r2
-    in r2
-    ceq r2, .S
-    cjnz %RS
-    ceq r2, .K
+    ^push BX
+    in BX
+    clr AX
+    ceq BX, .S
     cjnz %RK
-    mov r1, 2
+    ceq BX, .K
+    cadd AX, 1
+    cjnz %RK
+    add AX, 2
 #call("A")
-    mov r2, r1
+    mov BX, AX
 #call("read")
-    $(inline("sto", "r2", "r1"))
+    $(inline("sto", "BX", "AX"))
 #call("read")
-    inc r2
-    $(inline("sto", "r2", "r1"))
-    dec r2
-    ^mov r1, r2
-    in r2
-    pop r2
-    ret
-@RS
-    mov r1, 0
-#call("A")
-    pop r2
+    inc BX
+    $(inline("sto", "BX", "AX"))
+    dec BX
+    ^mov AX, BX
+    in BX
+    pop BX
     ret
 @RK
-    mov r1, 1
 #call("A")
-    pop r2
+    pop BX
     ret
 @eval
-    ^push r2
-    ^push r3
-    $(inline_r("rcl", "r2", "r1"))
-    jz r2, %skip
-    $(inline_r("rcl", "r3", "r2"))
-    jz r3, %skip
-    add r3, 2
-    $(inline_r("rcl", "r4", "r3"))
-    cne r4, 1
+    ^push CX
+    $(inline_r("rcl", "BX", "AX"))
+    jz BX, %skip
+    $(inline_r("rcl", "CX", "BX"))
+    jz CX, %skip
+    add CX, 2
+    $(inline_r("rcl", "DX", "CX"))
+    cne DX, 1
     cjnz %skip
-    inc r2
-    $(inline_r("rcl", "r3", "r2"))
-    mov r1, r3
-    mov f2, *changed
-    mov f3, 1
+    inc BX
+    $(inline_r("rcl", "CX", "BX"))
+    mov AX, CX
+    mov GX, *changed
+    mov HX, 1
 #call("sto_i")
     jmp %not_bi
 @skip
-    $(inline_r("rcl", "r2", "r1"))
-    jz r2, %notS
-    $(inline_r("rcl", "r3", "r2"))
-    jz r3, %notS
-    $(inline_r("rcl", "r4", "r3"))
-    jz r4, %notS
-    add r4, 2
-    $(inline_r("rcl", "r2", "r4"))
-    jnz r2, %notS
-    inc r1
-    $(inline_r("rcl", "r6", "r1"))
-    dec r1
-    $(inline_r("rcl", "r2", "r1"))
-    $(inline_r("rcl", "r3", "r2"))
-    inc r2
-    inc r3
-    $(inline_r("rcl", "r5", "r2"))
-    $(inline_r("rcl", "r4", "r3"))
-    mov r1, 2
+    $(inline_r("rcl", "BX", "AX"))
+    jz BX, %notS
+    $(inline_r("rcl", "CX", "BX"))
+    jz CX, %notS
+    $(inline_r("rcl", "DX", "CX"))
+    jz DX, %notS
+    add DX, 2
+    $(inline_r("rcl", "BX", "DX"))
+    jnz BX, %notS
+    inc AX
+    $(inline_r("rcl", "FX", "AX"))
+    dec AX
+    $(inline_r("rcl", "BX", "AX"))
+    $(inline_r("rcl", "CX", "BX"))
+    inc BX
+    inc CX
+    $(inline_r("rcl", "EX", "BX"))
+    $(inline_r("rcl", "DX", "CX"))
+    mov AX, 2
 #call("A")
-    ^mov r2, r1
-    mov r1, 2
+    ^mov BX, AX
+    mov AX, 2
 #call("A")
-    $(inline("sto", "r2", "r1"))
-    $(inline("sto", "r1", "r4"))
-    inc r1
-    $(inline("sto", "r1", "r6"))
-    mov r1, 2
+    $(inline("sto", "BX", "AX"))
+    $(inline("sto", "AX", "DX"))
+    inc AX
+    $(inline("sto", "AX", "FX"))
+    mov AX, 2
 #call("A")
-    inc r2
-    $(inline("sto", "r2", "r1"))
-    dec r2
-    $(inline("sto", "r1", "r5"))
-    inc r1
-    $(inline("sto", "r1", "r6"))
-    mov r1, r2
-    mov f2, *changed
-    mov f3, 1
+    inc BX
+    $(inline("sto", "BX", "AX"))
+    dec BX
+    $(inline("sto", "AX", "EX"))
+    inc AX
+    $(inline("sto", "AX", "FX"))
+    mov AX, BX
+    mov GX, *changed
+    mov HX, 1
 #call("sto_i")
     jmp %not_bi
 @notS
-    $(inline_r("rcl", "r2", "r1"))
-    cge r2, 2
+    $(inline_r("rcl", "BX", "AX"))
+    cge BX, 2
     cjz %not_bi
-    ^mov r3, r1
-    $(inline_r("rcl", "r1", "r3"))
+    ^mov CX, AX
+    $(inline_r("rcl", "AX", "CX"))
 #call("eval")
-    $(inline("sto", "r3", "r1"))
-    inc r3
-    $(inline_r("rcl", "r1", "r3"))
+    $(inline("sto", "CX", "AX"))
+    inc CX
+    $(inline_r("rcl", "AX", "CX"))
 #call("eval")
-    $(inline("sto", "r3", "r1"))
-    dec r3
-    ^mov r1, r3
+    $(inline("sto", "CX", "AX"))
+    dec CX
+    ^mov AX, CX
 @not_bi
-    pop r3
-    pop r2
+    pop CX
     ret
 
 #def_inline("sto")
